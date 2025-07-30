8a329a6e424e58b66e6b295fb1084f145d3fdf28
0-iQpy <cristiandave123456@gmail.com>
2025-07-30 08:24:34 +0800

    python backend

---
+++ webapp/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+app = FastAPI()
+
+# Set up Jinja2 template rendering
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    # Pass dynamic data to the template
+    return templates.TemplateResponse(
+        "home_page.html", {"request": request, "title": "Dynamic FastAPI Page"
+
+                           }
+        )
