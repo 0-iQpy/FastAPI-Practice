a10fa9cb129c20d147cba985d9e2b4272c775db2
0-iQpy <cristiandave123456@gmail.com>
2025-07-30 18:34:36 +0800

    some styling links

--- webapp/main.py
+++ webapp/main.py
@@ -2,18 +2,17 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Set up Jinja2 template rendering
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     # Pass dynamic data to the template
     return templates.TemplateResponse(
-        "index.html", {"request": request, "title": "Dynamic FastAPI Page"
-
-                       }
-        )
+        "index.html", {"request": request, "title": "Dynamic FastAPI
